92eecb4eaef001f65fa703b76b9a80df79197353
Diego Guimarães <diegobr.sistemas@gmail.com>
2015-11-01 13:44:25 -0200

    Point - Handling C data structures

--- pysample.c
+++ pysample.c
@@ -35,11 +35,58 @@ static PyObject *py_divide(PyObject *self, PyObject *args) {
     return Py_BuildValue("(ii)", quotient, remainder);
 }
 
+/* Destructor function for points */
+static void del_Point(PyObject *obj) {
+    free(PyCapsule_GetPointer(obj, "Point"));
+}
+
+/* Utility functions */
+static Point *PyPoint_AsPoint(PyObject *obj) {
+    return (Point *) PyCapsule_GetPointer(obj, "Point");
+}
+static PyObject *PyPoint_FromPoint(Point *p, int must_free) {
+    return PyCapsule_New(p, "Point", must_free ? del_Point : NULL);
+}
+
+/* Create a new Point object */
+static PyObject *py_Point(PyObject *self, PyObject *args) {
+    Point *p;
+    double x, y;
+    if (!PyArg_ParseTuple(args, "dd", &x, &y)) {
+        return NULL;
+    }
+    p = (Point *) malloc(sizeof(Point));
+    p->x = x;
+    p->y = y;
+    return PyPoint_FromPoint(p, 1);
+}
+
+/* Point - handling C data structure */
+static PyObject *py_distance(PyObject *self, PyObject *args) {
+    Point *p1, *p2;
+    PyObject *py_p1, *py_p2;
+    double result;
+
+    if (!PyArg_ParseTuple(args, "OO", &py_p1, &py_p2)) {
+        return NULL;
+    }
+    if (!(p1 = PyPoint_AsPoint(py_p1))) {
+        return NULL;
+    }
+    if (!(p2 = PyPoint_AsPoint(py_p2))) {
+        return NULL;
+    }
+    result  = distance(p1, p2);
+    return Py_BuildValue("d", result);
+}
+
 /* Module method table */
 static PyMethodDef SampleMethods[] = {
     {"gcd", py_gcd, METH_VARARGS, "Greatest common divisor"},
     {"in_mandel", py_in_mandel, METH_VARARGS, "Mandelbrot test"},
+    {"Point", py_Point, METH_VARARGS, "Make point"},
     {"divide", py_divide, METH_VARARGS, "Integer division"},
+    {"distance", py_distance, METH_VARARGS, "Distance between two points"},
     { NULL, NULL, 0, NULL }
 };
 
